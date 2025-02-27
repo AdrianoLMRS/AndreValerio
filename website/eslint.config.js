a684bea84664c01d3b9a9f4f0a0e7375982e01f7
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-02-27 16:49:04 -0300

    More basics config

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,29 +1,29 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+// import reactHooks from "eslint-plugin-react-hooks";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {
-        files: ['**/*.{js,mjs,cjs,ts}'],
-        languageOptions: {
-            globals: { ...globals.browser, ...globals.node }
-        },
-        rules: {
-            // ...reactHooks.configs.recommended.rules,
-            // "react-refresh/only-export-components": [
-            //   "warn",
-            //   { allowConstantExport: true }
-            // ],
-            'indent': ['warn', 4],
-            'quotes': ['warn', 'single'],
-            'semi': ['warn', 'always'],
-            'no-unused-vars': 'warn',
-            'no-console': 'warn',
-            'import/no-extraneous-dependencies': 'off'
-        }
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
     },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended
+    rules: {
+      // ...reactHooks.configs.recommended.rules,
+      // "react-refresh/only-export-components": [
+      //   "warn",
+      //   { allowConstantExport: true }
+      // ],
+      "indent": ["warn", 4],
+      "quotes": ["warn", "single"],
+      "semi": ["warn", "always"],
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "import/no-extraneous-dependencies": "off"
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
 ];
