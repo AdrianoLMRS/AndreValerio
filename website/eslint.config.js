5e3c3a9594370d5f96488182b5fefb2163150d41
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-03-02 16:37:47 -0300

    Fix eslint ignore

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,21 +1,40 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-// import reactHooks from "eslint-plugin-react-hooks";
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-/** @type {import('eslint').Linter.Config[]} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '../.gitignore');
+
+/** @type {import('eslint').Linter.Config} */
 export default [
+    includeIgnoreFile(gitignorePath),
     {
-        files: ['**/*.{js,mjs,cjs,ts}'],
+        // your overrides
+    },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,tsx}'],
         languageOptions: {
+            parser: tsParser, // Definir o parser do TypeScript
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
             globals: { ...globals.browser, ...globals.node }
         },
+
+        plugins: {
+            '@typescript-eslint': tsPlugin
+        },
+
         rules: {
-            // ...reactHooks.configs.recommended.rules,
-            // "react-refresh/only-export-components": [
-            //   "warn",
-            //   { allowConstantExport: true }
-            // ],
+            ...pluginJs.configs.recommended.rules,
+            ...tsPlugin.configs.recommended.rules, // Agora pega as regras corretamente
+
             'indent': ['warn', 4],
             'quotes': ['warn', 'single'],
             'semi': ['warn', 'always'],
@@ -23,7 +42,5 @@ export default [
             'no-console': 'warn',
             'import/no-extraneous-dependencies': 'off'
         }
-    },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended
+    }
 ];
