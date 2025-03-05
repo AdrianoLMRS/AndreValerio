36a566e2c02a07a2c9a3e3ed7f5cba293c92e2fe
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-03-05 11:43:06 -0300

    Color pallete changed

--- website/src/data/imgs.ts
+++ website/src/data/imgs.ts
@@ -7,10 +7,9 @@ const Imgs = {
         menu: '@assets/menu.svg',
     },
     cdn: {
-        background: `https://placehold.jp/60px/3d4070/FFFAFA/${backgroundSize}.png?text=Foto%20de%20background%20(Andr%C3%A9%20Val%C3%A9rio)&css=%7B%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%233d4070)%2C%20to(%234148ad))%22%7D`,
+        background: `https://placehold.jp/64/3d4070/D4CDF4/${backgroundSize}.png?text=Background%20Photo%20(Andr%C3%A9%20Val%C3%A9rio)&css=%7B%22background%22%3A%22%20linear-gradient(to%20bottom%2C%20%230A0A0A%2C%20%23333333)%22%7D`,
         menu: `https://img.icons8.com/ios-filled/${size}/menu--v6.png`,
         logo: `https://placehold.jp/FFFAFA/333333/${logoSize}.png?text=Logo&css=%7B%22border-radius%22%3A%2215px%22%7D`,
-        
     },
 };
 
