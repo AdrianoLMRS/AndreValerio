0018a6f014558390865a51cab162fd73647bde5b
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-03-09 15:42:49 -0300

    Style: Album.astro better

--- website/src/data/imgs.ts
+++ website/src/data/imgs.ts
@@ -11,7 +11,7 @@ const Imgs = {
         backgroundMobile: 'https://andrevalerio.com/wp-content/uploads/2023/10/Sambado_Quartet_credits_Alfredo_Alves-11.jpeg',
         menu: `https://img.icons8.com/ios-filled/${size}/menu--v6.png`,
         logo: `https://placehold.jp/FFFAFA/333333/${logoSize}.png?text=Logo&css=%7B%22border-radius%22%3A%2215px%22%7D`,
-        spotify: "https://img.icons8.com/ios-glyphs/56/spotify.png",
+        spotify: "https://img.icons8.com/ios-glyphs/48/spotify.png",
     },
 };
 
