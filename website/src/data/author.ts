a3ba1bd114b89de558ce990a8c2e5c366d33b61d
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-03-12 15:20:32 -0300

    Feat: Artist.astro component

--- website/src/data/author.ts
+++ website/src/data/author.ts
@@ -4,5 +4,4 @@ export const author = {
     job: 'Composer & Guitarist',
     about: 'Andre Valerio is a Brazilian multi-instrumentalist, guitarist, composer, and arranger. He has performed at renowned festivals and venues worldwide, blending jazz, Brazilian, and classical music. His debut album, Chrysalis (2017), showcases his talent and collaborations with esteemed musicians. With versatility and passion, Andre continues to captivate audiences globally.',
     spotifyId: '7bInuih8A0WSEqVwkbqXHN',
-
 };
