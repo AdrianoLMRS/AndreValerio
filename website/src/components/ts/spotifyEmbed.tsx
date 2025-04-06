7bf73a80f4c07bd094c1c98cc83b0ae83f2c2b93
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-04-06 15:36:22 -0300

    Feat + Style: loading animation #songs

---
+++ website/src/components/ts/spotifyEmbed.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState, type ReactNode } from 'react';
+
+type Props = {
+  html: string;
+  loading: ReactNode;
+};
+
+const defaultLoading = <div className="fallback">Loading...</div>;
+
+export default function SpotifyEmbed({ html, loading= defaultLoading }: Props) {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!wrapperRef.current) return;
+
+        const container = wrapperRef.current;
+        container.innerHTML = '';
+
+        const temp = document.createElement('div');
+        temp.innerHTML = html;
+
+        const iframe = temp.querySelector('iframe');
+        if (iframe) {
+            iframe.style.display = 'none'; // Hide until loads...
+            container.appendChild(iframe);
+
+            const observer = new MutationObserver(() => {
+                if (iframe && iframe.contentWindow) {
+                    setLoaded(true);
+                    iframe.style.display = 'block';
+                    observer.disconnect();
+                }
+            });
+
+            observer.observe(iframe, { attributes: true, childList: true, subtree: true });
+
+            // fallback: ativa o loading manualmente após 2 segundos
+            setTimeout(() => {
+                if (!loaded) {
+                    iframe.style.display = 'block';
+                    setLoaded(true);
+                    observer.disconnect();
+                }
+            }, 2000);
+        }
+    }, [html]);
+
+    return (
+        <div className="spotify-embed">
+            {!loaded && loading}
+            <div
+                ref={wrapperRef}
+                style={{ display: loaded ? 'contents' : 'none' }}
+            />
+        </div>
+    );
+}
