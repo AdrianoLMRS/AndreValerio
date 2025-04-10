a618bb65bb0f17c1cddf55f70804b75d2e38da57
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-04-10 17:25:12 -0300

    Feat: LD+Json for SEO

--- website/src/data/author.ts
+++ website/src/data/author.ts
@@ -1,10 +1,12 @@
 export const author = {
     name: 'André Valério',
+    altName: 'Valério Som',
     email: 'andrevalerio@gmail.com',
     job: 'Guitarist | Composer | Educator',
     about: 'Andre Valerio is a Brazilian multi-instrumentalist, guitarist, composer, and arranger. He has performed at renowned festivals and venues worldwide, blending jazz, Brazilian, and classical music. His debut album, Chrysalis (2017), showcases his talent and collaborations with esteemed musicians. With versatility and passion, Andre continues to captivate audiences globally.',
     spotifyId: '7bInuih8A0WSEqVwkbqXHN',
     site: 'https://andrevalerio.com/',
+    musicGenre: ['Brazilian Jazz', 'Samba', 'Choro'],
     dev: {
         name: 'Adriano Rossi',
         site: 'https://portfolio-adriano-p4dj.onrender.com/',
