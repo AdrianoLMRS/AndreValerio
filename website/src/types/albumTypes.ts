600c2c1411aaed4da9bafe4b3d1e7949d82f4e51
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-04-10 20:19:21 -0300

    Feat: Albums type & Schema (SEO)

--- website/src/types/albumTypes.ts
+++ website/src/types/albumTypes.ts
@@ -1,13 +1,38 @@
-// Album types
+interface SpotifyImage {
+    url: string
+    height?: number
+    width?: number
+}
+
+interface SpotifyArtist {
+    name: string
+    id: string
+    type: string
+    uri: string
+    external_urls: {
+        spotify: string
+    }
+}
+
 interface Album {
     id: string
     name: string
+    href: string
     release_date: string
-    images: { url: string }[]
+    release_date_precision: 'year' | 'month' | 'day'
+    images: SpotifyImage[]
+    external_urls: {
+        spotify: string
+    }
+    album_type: 'album' | 'single' | 'compilation'
+    album_group: 'album' | 'single' | 'appears_on' | 'compilation'
+    artists: SpotifyArtist[]
+    total_tracks: number
 }
 
 interface AlbumResponse {
-    items: Album[]  
+    items: Album[]
 }
-
-export type { Album, AlbumResponse };
+  
+export type { Album, AlbumResponse, SpotifyArtist, SpotifyImage }
+  
