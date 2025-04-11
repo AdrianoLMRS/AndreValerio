e2637d48461fba4875feea64389bd314ec2f1da7
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-04-10 21:15:14 -0300

    Feat: favicon.ico

--- website/src/data/imgs.ts
+++ website/src/data/imgs.ts
@@ -20,7 +20,7 @@ const Imgs = {
     cdn: {
         background: 'https://iili.io/3KXmjWP.webp',
         backgroundMobile: 'https://andrevalerio.com/wp-content/uploads/2023/10/Sambado_Quartet_credits_Alfredo_Alves-11.jpeg',
-        photo: 'https://andrevalerio.com/wp-content/uploads/2019/04/Andre_headshot-687x1024.jpg',
+        pic: 'https://iili.io/3cJZ2dF.jpg',
         menu: `https://img.icons8.com/ios-filled/${size}/menu--v6.png`,
         logo: `https://placehold.jp/FFFAFA/333333/${logoSize}.png?text=Logo&css=%7B%22border-radius%22%3A%2215px%22%7D`,
         spotify: 'https://img.icons8.com/ios-glyphs/24/spotify.png',
