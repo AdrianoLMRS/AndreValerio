c332cc3ea3f1352e56d0586db3342657b67709c8
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-05-12 15:24:22 -0300

    Style: /contact social links

--- website/src/data/imgs.ts
+++ website/src/data/imgs.ts
@@ -34,7 +34,7 @@ const Imgs = {
         menu: `https://img.icons8.com/ios-filled/${size}/menu--v6.png`,
         logo: `https://placehold.jp/FFFAFA/333333/${logoSize}.png?text=Logo&css=%7B%22border-radius%22%3A%2215px%22%7D`,
         phone: `https://img.icons8.com/ios-filled/${socialSize}/phone.png`,
-        email: `https://img.icons8.com/deco-glyph/${socialSize}/new-post.png`,
+        email: `https://img.icons8.com/ios-filled/${socialSize}/new-post.png`,
         whatsapp: `https://img.icons8.com/ios-filled/${socialSize}/whatsapp--v1.png`,
         spotify: 'https://img.icons8.com/ios-glyphs/24/spotify.png',
         linktree: `https://img.icons8.com/pulsar-gradient/${socialSize}/linktree.png`,
