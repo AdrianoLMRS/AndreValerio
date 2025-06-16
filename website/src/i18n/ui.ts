73f81c488ab72ed89ccaf7d989684749cb0d8de6
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-06-16 15:43:04 -0300

    Feat: /de page test

--- website/src/i18n/ui.ts
+++ website/src/i18n/ui.ts
@@ -1,3 +1,17 @@
+const modules = import.meta.glob('./locales/*.json', { eager: true });
+
+// This function flattens a nested object into a single-level object with dot notation for keys.
+function flatten(obj: Record<string, string>, prefix = ''): Record<string, string> {
+    return Object.entries(obj).reduce((acc, [key, val]) => {
+        const fullKey = prefix ? `${prefix}.${key}` : key;
+        return typeof val === 'object' && val !== null ? Object.assign(acc, flatten(val, fullKey)) : { ...acc, [fullKey]: val };
+    }, {} as Record<string, string>);
+};
+
+// This function normalizes the language code by removing the './locales/' prefix and '.json' suffix
+// e.g: './locales/en.json' becomes 'en' and './locales/pt-BR.json' becomes 'pt_BR'.
+const normalize = (code: string) => code.replace('./locales/', '').replace('.json', '').replace('-', '_');
+
 export const languages = {
     en: 'English',
     pt: 'Português',
@@ -10,15 +24,10 @@ export const languages = {
 
 export const defaultLang = 'en';
 
-export const ui = {
-    en: {
-        'home.title': 'André Valério - Guitarist | Composer | Educator',
-        'home.description': 'André Valério Official website, Guitarist | Composer | Educator. Discover my music, videos, and more.',
-        'home.imgAlt': 'André Valério official banner',
-    },
-    de: {
-        'home.title': 'André Valério - Gitarrist | Komponist | Pädagoge',
-        'home.description': 'Offizielle Website von André Valério, Gitarrist | Komponist | Pädagoge. Entdecken Sie meine Musik, Videos und mehr.',
-        'home.imgAlt': 'André Valério offizielles Banner',
-    },
-} as const;
+export const ui = Object.entries(modules).reduce((acc, [path, mod]) => {
+    const langCode = normalize(path);
+    acc[langCode] = flatten((mod as { default: Record<string, string> }).default);
+    return acc;
+}, {} as Record<string, Record<string, string>>);
+
+console.debug(ui);
