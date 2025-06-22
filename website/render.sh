375878ef76641eaf1a459dc9d38f5190eb80e659
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-06-22 13:55:48 -0300

    Fix: render.sh GIT branch

--- website/render.sh
+++ website/render.sh
@@ -12,24 +12,38 @@ if [ -z "$GITHUB_PAT" ]; then
   exit 1
 fi
 
-echo "🔎 Searching for artifact ID..."
+echo "🔍 Searching last success run in branch 'internationalization'..."
 
-ARTIFACT_ID=$(curl -s -H "Authorization: token $GITHUB_PAT" \
-  https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/artifacts |
-  grep -A 10 '"name": "site-dist"' | grep '"id":' | head -n 1 | sed 's/[^0-9]*//g')
+RUN_ID=$(curl -s -H "Authorization: token $GITHUB_PAT" \
+  "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/runs?branch=internationalization&status=success&per_page=1" |
+  grep '"id":' | head -n 1 | sed 's/[^0-9]*//g')
 
-if [ -z "$ARTIFACT_ID" ]; then
+if [ -z "$RUN_ID" ]; then
+  echo "❌ No success run found in 'internationalization'."
+  exit 1
+fi
+
+echo "🔎 Searching for artifact ID ($RUN_ID)..."
+
+ARTIFACT_URL=$(curl -s -H "Authorization: token $GITHUB_PAT" \
+  "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/runs/$RUN_ID/artifacts" |
+  grep -B 5 '"name": "site-dist"' |
+  grep '"archive_download_url":' |
+  head -n 1 |
+  cut -d '"' -f 4)
+
+if [ -z "$ARTIFACT_URL" ]; then
   echo "❌ Artifact 'site-dist' not found..."
   exit 1
 fi
 
-echo "📦 Artifact ID: $ARTIFACT_ID"
+echo "📦 Artifact URL: $ARTIFACT_URL"
 echo "⬇️ Downloading & extracting dist..."
 
 rm -rf dist
 curl -L -H "Authorization: token $GITHUB_PAT" \
   -H "Accept: application/vnd.github+json" \
-  "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/artifacts/$ARTIFACT_ID/zip" \
+  "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/artifacts/$ARTIFACT_URL/zip" \
   -o site-dist.zip
 
 unzip site-dist.zip -d dist
