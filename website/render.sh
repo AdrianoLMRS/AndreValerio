5562ea470c7b064f447306e5f41ed0611149ea7d
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-10-04 18:28:57 -0300

    Fix: Compare 2 workflows dates

--- website/render.sh
+++ website/render.sh
@@ -16,22 +16,45 @@ if [ -z "$WORKFLOW_ID" ]; then
     exit 1
 fi
 
+REPO="AdrianoLMRS/AndreValerio"
+BRANCH="main"
+
 echo "🔍 Searching last success run"
 
-RUN_ID=$(curl -s -H "Authorization: token $GITHUB_PAT" \
-    "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/runs?status=success&per_page=1" \
-    | jq '.workflow_runs[0].id')
+WORKFLOW1_ID=$WORFLOW_ID_1
+WORKFLOW2_ID=$WORFLOW_ID_2
 
+get_last_success() {
+    local workflow_id=$1
+    curl -s -H "Authorization: token $GITHUB_PAT" \
+        "https://api.github.com/repos/$REPO/actions/workflows/$workflow_id/runs?branch=$BRANCH&status=success&per_page=1" \
+        | jq -r '.workflow_runs[0] | {id, created_at}'
+}
+
+W1_ID=$(echo "$W1" | jq -r '.id')
+W1_DATE=$(echo "$W1" | jq -r '.created_at')
+
+W2_ID=$(echo "$W2" | jq -r '.id')
+W2_DATE=$(echo "$W2" | jq -r '.created_at')
+
+# Compare timestamps
+if [[ "$W1_DATE" > "$W2_DATE" ]]; then
+    RUN_ID=$W1_ID
+    CHOSEN_DATE=$W1_DATE
+else
+    RUN_ID=$W2_ID
+    CHOSEN_DATE=$W2_DATE
+fi
 
 if [ -z "$RUN_ID" ]; then
-    echo "❌ No success run found in 'internationalization'."
+    echo "❌ No success run found in $BRANCH..."
     exit 1
 fi
 
 echo "🔎 Searching for artifact ID ($RUN_ID)..."
 
 ARTIFACT_ID=$(curl -s -H "Authorization: token $GITHUB_PAT" \
-    "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/runs/$RUN_ID/artifacts" | \
+    "https://api.github.com/repos/$REPO/actions/runs/$RUN_ID/artifacts" | \
     jq -r '.artifacts[] | select(.name=="site-dist") | .id')
 
 if [ -z "$ARTIFACT_ID" ]; then
@@ -45,7 +68,7 @@ echo "⬇️ Downloading & extracting dist..."
 rm -rf dist
 curl -L -H "Authorization: token $GITHUB_PAT" \
     -H "Accept: application/vnd.github+json" \
-    "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/artifacts/$ARTIFACT_ID/zip" \
+    "https://api.github.com/repos/$REPO/actions/artifacts/$ARTIFACT_ID/zip" \
     -o site-dist.zip
 
 unzip site-dist.zip -d dist
