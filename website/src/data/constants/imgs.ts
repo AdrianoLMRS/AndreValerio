d242cf0527c4934629a50d066504d33c757310b8
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-11-03 20:39:19 -0300

    Feat: Add all new photos!

--- website/src/data/constants/imgs.ts
+++ website/src/data/constants/imgs.ts
@@ -2,6 +2,29 @@ import Banner from '@assets/img/Andre-Valerio-Banner.webp';
 import Banner2 from '@assets/img/Andre-Valerio-Banner-2.webp';
 import Banner3 from '@assets/img/Andre-Valerio-Banner-3.webp';
 import Banner4 from '@assets/img/Andre-Valerio-Banner-4.webp';
+import AndreValerioBigTreeFlorest1 from '@assets/img/Andre-Valerio-Big-Tree-Florest-1.webp';
+import AndreValerioBigTreeFlorest2 from '@assets/img/Andre-Valerio-Big-Tree-Florest-2.webp';
+import AndreValerioBigTreeFlorest3 from '@assets/img/Andre-Valerio-Big-Tree-Florest-3.webp';
+import AndreValerioHeadshot from '@assets/img/Andre-Valerio-Headshot.webp';
+import AndreValerioIntro from '@assets/img/Andre-Valerio-Intro.webp';
+import AndreValerioPlayingGuitarFlorest1 from '@assets/img/Andre-Valerio-Playing-Guitar-Florest-1.webp';
+import AndreValerioPlayingGuitarFlorest2 from '@assets/img/Andre-Valerio-Playing-Guitar-Florest-2.webp';
+import AndreValerioPlayingGuitarFlorest3 from '@assets/img/Andre-Valerio-Playing-Guitar-Florest-3.webp';
+import AndreValerioPlayingGuitarFlorest4 from '@assets/img/Andre-Valerio-Playing-Guitar-Florest-4.webp';
+import AndreValerioPlayingGuitarFlorest5 from '@assets/img/Andre-Valerio-Playing-Guitar-Florest-5.webp';
+import AndreValerioPlayingGuitarFlorest6 from '@assets/img/Andre-Valerio-Playing-Guitar-Florest-6.webp';
+import AndreValerioSocialMediaprofilepicture from '@assets/img/Andre-Valerio-Social-Media-profile-picture.webp';
+import AndreValeriobesidefence from '@assets/img/Andre-Valerio-beside-fence.webp';
+import AndreValeriobesidetree from '@assets/img/Andre-Valerio-beside-tree.webp';
+import AndreValerioflorestportrait1 from '@assets/img/Andre-Valerio-florest-portrait-1.webp';
+import AndreValerioflorestportrait2 from '@assets/img/Andre-Valerio-florest-portrait-2.webp';
+import AndreValerioflorest from '@assets/img/Andre-Valerio-florest.webp';
+import AndreValeriohead from '@assets/img/Andre-Valerio-head.webp';
+import AndreValerioplayingguitar from '@assets/img/Andre-Valerio-playing-guitar.webp';
+import AndreValerioposing1 from '@assets/img/Andre-Valerio-posing-1.webp';
+import AndreValerioposing2 from '@assets/img/Andre-Valerio-posing-2.webp';
+import AndreValeriositdown1 from '@assets/img/Andre-Valerio-sit-down-1.webp';
+import AndreValeriositdown2 from '@assets/img/Andre-Valerio-sit-down-2.webp';
 
 const n : number = 48;
 const size : string = String(n);
@@ -16,6 +39,29 @@ const Imgs = {
         banner2: Banner2,
         banner3: Banner3,
         banner4: Banner4,
+        AndreValerioBigTreeFlorest1: AndreValerioBigTreeFlorest1,
+        AndreValerioBigTreeFlorest2: AndreValerioBigTreeFlorest2,
+        AndreValerioBigTreeFlorest3: AndreValerioBigTreeFlorest3,
+        AndreValerioHeadshot: AndreValerioHeadshot,
+        AndreValerioIntro: AndreValerioIntro,
+        AndreValerioPlayingGuitarFlorest1: AndreValerioPlayingGuitarFlorest1,
+        AndreValerioPlayingGuitarFlorest2: AndreValerioPlayingGuitarFlorest2,
+        AndreValerioPlayingGuitarFlorest3: AndreValerioPlayingGuitarFlorest3,
+        AndreValerioPlayingGuitarFlorest4: AndreValerioPlayingGuitarFlorest4,
+        AndreValerioPlayingGuitarFlorest5: AndreValerioPlayingGuitarFlorest5,
+        AndreValerioPlayingGuitarFlorest6: AndreValerioPlayingGuitarFlorest6,
+        AndreValerioSocialMediaprofilepicture: AndreValerioSocialMediaprofilepicture,
+        AndreValeriobesidefence: AndreValeriobesidefence,
+        AndreValeriobesidetree: AndreValeriobesidetree,
+        AndreValerioflorestportrait1: AndreValerioflorestportrait1,
+        AndreValerioflorestportrait2: AndreValerioflorestportrait2,
+        AndreValerioflorest: AndreValerioflorest,
+        AndreValeriohead: AndreValeriohead,
+        AndreValerioplayingguitar: AndreValerioplayingguitar,
+        AndreValerioposing1: AndreValerioposing1,
+        AndreValerioposing2: AndreValerioposing2,
+        AndreValeriositdown1: AndreValeriositdown1,
+        AndreValeriositdown2: AndreValeriositdown2,
     },
     cdn: {
         menu: `https://img.icons8.com/ios-filled/${size}/menu--v6.png`,
